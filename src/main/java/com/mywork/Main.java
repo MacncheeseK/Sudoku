77da7253add0cc29a2a3126a1705dd1598df7cd4
DM <danymartinezluna@gmail.com>
2023-07-23 18:54:23 -0400

    Added checkSquare() function

--- src/main/java/com/mywork/Main.java
+++ src/main/java/com/mywork/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     // Creates the Sudoku Board
     public static void generateBorad(){
-       int[][] sudokuBoard = board(40);
+       int[][] sudokuBoard = board(30);
         for(int Row=0; Row<9;Row++) {
             if(Row%3==0 && Row!=0) {
                 System.out.print("---------+---------+---------\n");
@@ -25,14 +25,14 @@ public class Main {
     public static int[][] board(int Difficulty){
         int[][] grid =new int[9][9];
         int row,col,genNum;
-        for(int i=0; i<Difficulty;i++){
+        for(int intrations =0; intrations<Difficulty;intrations++){
             do{
                 row= (int)(Math.random()*9);
                 col= (int)(Math.random()*9);
             }while(grid[row][col]!=0);
             do{
                 genNum=(int)(1+ Math.random()*9);
-            }while(!(checkRow(grid,genNum,row) && checkCol(grid,genNum,col)));
+            }while(!(checkGrid(grid,genNum,row,col) ));
 
 
 
@@ -46,16 +46,28 @@ public class Main {
     public static boolean checkRow(int[][] grid, int num, int row){
         for(int col=0 ;col<9;col++){
             if(grid[row][col]==num) return false;
+
         }
         return true;
     }
     public static boolean checkCol(int[][] grid, int num, int col) {
         for(int row=0;row<9;row++){
             if(grid[row][col]==num) return false;
+
+        }
+        return true;
+    }
+    public static boolean checkSquare(int[][] grid, int num, int row, int col){
+        int rowSquare = (Math.floorDiv(row,3))*3;
+        int colSquare = (Math.floorDiv(col,3))*3 ;
+        for(int i=0;i<3;i++){
+            for(int j =0; j<3;j++){
+                if(grid[rowSquare+i][colSquare+j] == num) return false;
+            }
         }
         return true;
     }
     public static boolean checkGrid(int[][] grid, int num,int row, int col){
-        return checkCol(grid,num,col) && checkRow(grid, num,row);
+        return checkCol(grid,num,col) && checkRow(grid, num,row) && checkSquare(grid,num,row,col);
     }
 }
